a016f137e3409581e611ce1f57cfcf4015b0e817
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-01-17 12:34:06 +0100

    Update greeting message in hello_world.js to be more relevant

--- hello-world/js/hello_world.js
+++ hello-world/js/hello_world.js
@@ -3,4 +3,4 @@ let age = 21;
 var country = "France";
 console.log("Hello, My name is " + name + " and I am " + age + " years old. I live in " + country + ".");
 
-document.body.innerHTML += "<h1>Hello, World?????????????</h1>";
+document.body.innerHTML += "<h3>What is this ?</h3>";
