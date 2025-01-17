16f7004c7d9ae5f28681d00c09034a2a56a7e515
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-01-17 13:32:22 +0100

    Enhance hello_world.js with additional logic and improved name formatting

--- hello-world/js/hello_world.js
+++ hello-world/js/hello_world.js
@@ -1,6 +1,28 @@
-const name = "hamza";
+const name = "Hamza";
 let age = 21;
 var country = "France";
+nb1 = 15;
+nb2 = 2;
+let i = 0;
 console.log("Hello, My name is " + name + " and I am " + age + " years old. I live in " + country + ".");
+console.log(nb1 + nb2);
 
+if (age >= 18) 
+{
+    console.log("You are over 18.");
+} else 
+{
+    console.log("You are under 18.");
+}
+
+while (i <= 5)
+{
+    console.log("count = " + i);
+    i++;
+}
+
+for (;i <= 10; i++)
+{
+    console.log("count = " + i);
+}
 document.body.innerHTML += "<h3>What is this ?</h3>";
