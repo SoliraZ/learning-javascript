7991003db6b2d7048159b58e691edadeffa5ce4c
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-01-17 12:29:54 +0100

    Add initial HTML and JavaScript files for Hello World program

---
+++ hello-world/js/hello_world.js
@@ -0,0 +1,6 @@
+const name = "hamza";
+let age = 21;
+var country = "France";
+console.log("Hello, My name is " + name + " and I am " + age + " years old. I live in " + country + ".");
+
+document.body.innerHTML += "<h1>Hello, World?????????????</h1>";
